0f754c6829e50859a874a655d13ca900a43a0999
Waaannns <146621734+Waaannns@users.noreply.github.com>
2025-05-04 20:52:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import httpx, requests as r
+import httpx
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
